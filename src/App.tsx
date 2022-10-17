33d126b09ca68cb5babe59bdf29d962a775253a1
Louis Collard <louiscollard2@gmail.com>
2022-10-17 12:00:44 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+const App: FunctionComponent = () => {
+	const name: String = "React";
+
+	return <h1>Bonjour, {name} !</h1>;
+};
+
+export default App;
